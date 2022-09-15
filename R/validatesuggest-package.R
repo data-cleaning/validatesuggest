b51a1a1f2490da68a6d7e4143c3e770ac6ce43fd
Edwin de Jonge <edwindjonge@gmail.com>
2022-09-15 17:03:44 +0200

    adding vignette ;

--- R/validatesuggest-package.R
+++ R/validatesuggest-package.R
@@ -1,5 +1,10 @@
 #' @keywords internal
 #' @importFrom utils packageVersion
+#' @section validatesuggest:
+#' The goal of validatesuggest is to generate suggestions for
+#' validation rules from a supplied
+#' dataset. These can be used as a starting point for a rule set and are to be
+#' adjusted by domain experts.
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
